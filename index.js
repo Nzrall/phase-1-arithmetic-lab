8787cf5f5964e093c9107788ba1494155d7c802e
Joshua <wilson.joshua@outlook.com>
2021-09-03 16:19:48 -0600

    commit tree a5cadac76b4a22daa5a5ec2ee241759a732e4720

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() +1)
+
+let num3 = 32
+let num4 = 8
+
+const mod = num3 / num4
+
+
+
+const max = Math.max(1, 20)
